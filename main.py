bd4174ce1d061016c39b4881ebb4980c487eed53
sfzhou567hp <260174969@qq.com>
2019-03-18 21:39:48 +0800

    1. 加入根据star更新user_stack 2. 加入了大量新的user_agents
    TODO:
    1. 多线程
    2. download&clean
    3. db

--- main.py
+++ main.py
@@ -4,29 +4,60 @@ import time
 import zipfile
 import urllib.request
 import socket
+import random
+
+agents = [
+  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
+  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
+  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'
+  'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+  'Opera/9.25 (Windows NT 5.1; U; en)',
+  'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+  'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+  'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+  'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+  "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+  "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 ",
+]
 
 
 def read_url(url):
   req = urllib.request.Request(url)
-  req.add_header('User-Agent',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')
+  req.add_header('User-Agent', agents[random.randint(0, len(agents) - 1)])
 
   page_source = urllib.request.urlopen(req).read()
   page_source = page_source.decode("UTF-8")
   return page_source
 
 
-def get_repos(user_name):
-  url = 'https://api.github.com/users/%s/repos' % user_name
+def get_repos(user_name, type):
+  """
+  type=['repos','starred']
+  1. 获取某个user的repos信息: https://api.github.com/users/{user_name}/repos
+  2. 获取某用户所有star了的repo的信息(和2相反):https://api.github.com/users/{user_name}/starred
+  :param user_name: 
+  :param type: 
+  :return: 
+  """
+  url = 'https://api.github.com/users/%s/%s' % (user_name, type)
+  # repos = json.loads(read_url(url))
   try:
     repos = json.loads(read_url(url))
   except:
     # TODO: 记录错误记录
     repos = []
+    print('Get repos error_%s_%s' % (user_name, type))
+    time.sleep(3)
   return repos
 
 
 def process_user(user_name):
+  """
+  将user信息存入DB
+  :param user_name: 
+  :return: 
+  """
   pass
 
 
@@ -113,6 +144,7 @@ def process_repo(base_folder, repo, user_name_set, processed_repo_set,
   create_time = transform_datetime(repo['created_at'])
   update_time = transform_datetime(repo['updated_at'])
 
+  ## 因为要做personal, 所以就不舍star的阈值了, 不过之后可以在db里根据star数进行筛选
   star_cnt = repo['stargazers_count']
   fork_cnt = repo['forks_count']
 
@@ -127,7 +159,7 @@ def process_repo(base_folder, repo, user_name_set, processed_repo_set,
   # clean_repo_files(true_save_path)  ## local_save_path目录里将会有一个名为{repo_name}_{branch}的文件夹, 需要过滤文件并且删除该文件夹
   ## TODO: 2. save repo infos
 
-  ## TODO: 3. 获取star了当前repo的所有用户的信息
+  ## 3. 获取star了当前repo的所有用户的信息
   stargazers_url = repo['stargazers_url']
   process_stargazers(stargazers_url, user_name_set)
 
@@ -142,14 +174,12 @@ if __name__ == '__main__':
   base_folder = r'D:\DeeplearningData\GithubCode\Java'
   if not os.path.exists(base_folder):
     os.makedirs(base_folder)
-  # seed_users = ['gaopu', 'sfzhou5678']
-  seed_users = ['gaopu']
+  seed_users = ['gaopu', 'sfzhou5678']
 
   ## TODO: 各种set, stack封装成类
   user_name_set = set()
   processed_repo_set = set()
   user_stack = []  # TODO: 处理一下star和repo的交互逻辑
-  repo_stack = []
 
   for user_name in seed_users:
     if user_name not in user_name_set:
@@ -159,8 +189,16 @@ if __name__ == '__main__':
       user_stack.append(user_name)
 
   while len(user_stack):
+    """
+    每次拿出一个user, 做的事情有:
+    1. 获取这个user所有star了的项目star_repos
+    2. 获取当前用户自己的仓库repos
+    3. repos+=star_repos, 然后process repo(在这里下载当前repo(到owner的id目录), 同时将该项目的stargazers都加入user_stack)
+    """
     print(len(user_stack))
     user_name = user_stack.pop()
-    repos = get_repos(user_name)
+    star_repos = get_repos(user_name, 'starred')
+    repos = get_repos(user_name, 'repos')
+    repos += star_repos
     for repo in repos:
       process_repo(base_folder, repo, user_name_set, processed_repo_set, target_language='Java')
--- old/download.py
+++ old/download.py
@@ -5,57 +5,58 @@ import os
 from bs4 import BeautifulSoup
 import zipfile
 
+
 def get_proj_urls(bs_obj):
-    """
-    传入bsobj形式的ul主体，返回ul中所有li下的a标签的href
-    :param bs_obj:
-    :return:
-    """
-    project_urls=[]
-    project_divs=bs_obj.find_all('div',{'class':'d-inline-block col-9 mb-1'})
-    for project in project_divs:
-        url=project.find('a',{'class':'v-align-middle'})['href']
-        project_urls.append(url)
-    return project_urls
+  """
+  传入bsobj形式的ul主体，返回ul中所有li下的a标签的href
+  :param bs_obj:
+  :return:
+  """
+  project_urls = []
+  project_divs = bs_obj.find_all('div', {'class': 'd-inline-block col-9 mb-1'})
+  for project in project_divs:
+    url = project.find('a', {'class': 'v-align-middle'})['href']
+    project_urls.append(url)
+  return project_urls
 
 
 def get_repo_list(page_source):
-    """
-    传入页面源码，返回bsobj形式的ul主体(repo_list)
-    :param page_source:
-    :return:
-    """
-    bs_obj=BeautifulSoup(page_source)
-    repo_list=bs_obj.find('ul',{'class':'repo-list js-repo-list'})
-    return repo_list
+  """
+  传入页面源码，返回bsobj形式的ul主体(repo_list)
+  :param page_source:
+  :return:
+  """
+  bs_obj = BeautifulSoup(page_source)
+  repo_list = bs_obj.find('ul', {'class': 'repo-list js-repo-list'})
+  return repo_list
 
 
-def download_projects(project_urls,download_folder_path,unzip=False):
-    baseurl=r'https://codeload.github.com'
-    # https://codeload.github.com/kingname/python/zip/master
-    # /kingname/python
-    for url in project_urls:
-        download_url=baseurl+url+r'/zip/master'
-        try:
-            r = urllib.request.urlopen(download_url)
+def download_projects(project_urls, download_folder_path, unzip=False):
+  baseurl = r'https://codeload.github.com'
+  # https://codeload.github.com/kingname/python/zip/master
+  # /kingname/python
+  for url in project_urls:
+    download_url = baseurl + url + r'/zip/master'
+    try:
+      r = urllib.request.urlopen(download_url)
 
-            # download zip
-            filepath=os.path.join(download_folder_path,url[1:].replace(r'/','-'))+r'.zip'
-            f = open(filepath, 'wb')
-            f.write(r.read())
-            f.close()
-            # print('download completed')
+      # download zip
+      filepath = os.path.join(download_folder_path, url[1:].replace(r'/', '-')) + r'.zip'
+      f = open(filepath, 'wb')
+      f.write(r.read())
+      f.close()
+      # print('download completed')
 
-            if(unzip):
-                # unzip
-                zipf =  zipfile.ZipFile(filepath,'r')
-                for file in zipf.namelist():
-                    zipf.extract(file,filepath.replace(r'.zip',''))
-                zipf.close()
-                # print('unzip conpleted')
+      if (unzip):
+        # unzip
+        zipf = zipfile.ZipFile(filepath, 'r')
+        for file in zipf.namelist():
+          zipf.extract(file, filepath.replace(r'.zip', ''))
+        zipf.close()
+        # print('unzip conpleted')
 
-                # delete zip
-                os.remove(filepath)
-            # print(filepath+"  completed")
-        except:
-            print(url+' error')
+        # delete zip
+        os.remove(filepath)
+        # print(filepath+"  completed")
+    except:
+      print(url + ' error')
