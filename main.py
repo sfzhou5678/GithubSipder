c1a610082c64c44f28ceb0e09644bee211a02b88
sfzhou567hp <260174969@qq.com>
2019-03-18 22:44:40 +0800

    1. 加入了proxies(ip池)，但是好像加了代理之后全都是forbidden403了
    TODO:
    1. 老版手工爬虫不要用github api?
    2. download&clean
    3. db

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import urllib.request
 import socket
 import random
 import threading
+from urllib.error import URLError, HTTPError
+from bs4 import BeautifulSoup
+import requests
 
 # agents = [
 #   'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
@@ -24,15 +27,66 @@ import threading
 # ]
 with open('data/user_agents.txt') as f:
   agents = [line.strip() for line in f.readlines()]
+proxies = []
+overdue_ips = set()
 
 
-def read_url(url):
-  time.sleep(0.5)  # 每次都延缓一下
-  req = urllib.request.Request(url)
-  req.add_header('User-Agent', agents[random.randint(0, len(agents) - 1)])
+def update_proxies():
+  print('Update proxies...')
+  global proxies
+  global overdue_ips
 
-  page_source = urllib.request.urlopen(req).read()
-  page_source = page_source.decode("UTF-8")
+  for page in range(3):
+    url = 'http://www.xicidaili.com/nn/%s' % ('' if page == 0 else str(page + 1))
+
+    headers = {'User-Agent': agents[random.randint(0, len(agents) - 1)]}
+    web_data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(web_data.text, 'lxml')
+    ips = soup.find_all('tr')
+    for i in range(1, len(ips)):
+      ip_info = ips[i]
+      tds = ip_info.find_all('td')
+      ip = 'http://' + tds[1].text + ':' + tds[2].text
+      if ip not in overdue_ips and ip not in proxies:
+        proxies.append(ip)
+  print('Available proxies num:', len(proxies))
+
+
+def get_random_proxy():
+  global proxies
+  proxy_ip = random.choice(proxies)
+  proxy = {'http': proxy_ip}
+  return proxy
+
+
+def read_url(url, use_proxy=False):
+  time.sleep(1)  # 每次都延缓一下
+  if not use_proxy:
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent', agents[random.randint(0, len(agents) - 1)])
+    page_source = urllib.request.urlopen(req).read()
+    page_source = page_source.decode("UTF-8")
+  else:
+    if len(proxies) <= 10:
+      update_proxies()
+    page_source = '[]'
+    while len(proxies) > 0:
+      header = {'User-Agent': agents[random.randint(0, len(agents) - 1)]}
+      proxy = get_random_proxy()
+      proxy_handler = urllib.request.ProxyHandler(proxy)
+      opener = urllib.request.build_opener(proxy_handler)
+      request = urllib.request.Request(url, headers=header)
+      try:
+        page_source = opener.open(request).read().decode('utf-8')
+        break
+      except HTTPError as e:
+        print('HTTP Error, error code: ', e.code)
+        proxies.remove(proxy['http'])
+        overdue_ips.add(proxy['http'])
+      except URLError as e:
+        print('Url error, Reason: ', e.reason)
+        return '[]'
+
   return page_source
 
 
